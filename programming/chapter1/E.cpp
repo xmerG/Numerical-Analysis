eb82c09c387f9393af40d306434229a3059015c6
GMSSSM <124522778+xmerG@users.noreply.github.com>
2024-10-06 16:24:53 +0800

    Update E.cpp

--- programming/chapter1/E.cpp
+++ programming/chapter1/E.cpp
@@ -13,13 +13,13 @@ class F1:public Function{
 };
 
 int main(){
-    Bisection_Method h1(F1(), 0.0, 0.5, 1e-8,0.01);
-    cout<<"the height of water is "<<h1.solve()<<endl;
+    Bisection_Method h1(F1(), 0.0, 0.5, 1e-8,0.02);
+    cout<<"the height of water is "<<1.0-h1.solve()<<endl;
 
-    Newton_Method h2(F1(),0.0);
-    cout<<"the height of water is "<<h2.solve()<<endl;
+    Newton_Method h2(F1(),0.14,0.00025,100);
+    cout<<"the height of water is "<<1.0-h2.solve()<<endl;
 
-    Secant_Method h3(F1(),0.0 ,1,0 ,0.01);
-    cout<<"the height of water is "<<h3.solve()<<endl;
+    Secant_Method h3(F1(),0.14 ,0.17 ,0.001,200);
+    cout<<"the height of water is "<<1.0-h3.solve()<<endl;
     return 0;
 }
